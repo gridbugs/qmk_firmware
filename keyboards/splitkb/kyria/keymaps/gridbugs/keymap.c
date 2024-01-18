c8be02c5039d0c7d93fc64fde7558a88112cfacf
Stephen Sherratt <stephen@sherra.tt>
2024-01-18 14:39:13 +1100

    tidy

--- keyboards/splitkb/kyria/keymaps/gridbugs/keymap.c
+++ keyboards/splitkb/kyria/keymaps/gridbugs/keymap.c
@@ -13,32 +13,22 @@ const rgblight_segment_t PROGMEM my_layer0_layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {11, 1, HSV_GREEN}
 );
 
-const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {11, 10, HSV_RED}
-);
-
 const rgblight_segment_t PROGMEM my_layer3_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 62, HSV_PURPLE}
+    {0, 62, HSV_MAGENTA}
 );
 const rgblight_segment_t PROGMEM my_layer4_layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {0, 62, HSV_CYAN}
 );
 const rgblight_segment_t PROGMEM my_layer5_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 62, HSV_MAGENTA}
+    {0, 62, HSV_GREEN}
 );
 
 // Now define the array of layers. Later layers take precedence
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-//    my_layer0_layer,
-    my_layer1_layer,
-    my_layer1_layer,
-    my_layer1_layer,
-    my_layer1_layer,
+    my_layer0_layer,
+    my_layer3_layer,
     my_layer4_layer,
-    my_layer1_layer
-//    my_layer3_layer,
-//    my_layer4_layer,
-//    my_layer5_layer
+    my_layer5_layer
 );
 
 void keyboard_post_init_user(void) {
@@ -47,20 +37,13 @@ void keyboard_post_init_user(void) {
 }
 
 layer_state_t default_layer_state_set_user(layer_state_t state) {
-    rgblight_set_layer_state(5, layer_state_cmp(state, 0));
+    rgblight_set_layer_state(0, layer_state_cmp(state, 0));
     return state;
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-   // rgblight_set_layer_state(0, layer_state_cmp(state, 0))
-    rgblight_set_layer_state(5, layer_state_cmp(state, 0));
-    rgblight_set_layer_state(4, layer_state_cmp(state, 1));
-    rgblight_set_layer_state(3, layer_state_cmp(state, 2));
-    rgblight_set_layer_state(2, layer_state_cmp(state, 3));
-    rgblight_set_layer_state(1, layer_state_cmp(state, 4));
-    rgblight_set_layer_state(0, layer_state_cmp(state, 5));
-   // rgblight_set_layer_state(2, layer_state_cmp(state, 3));
-   // rgblight_set_layer_state(3, layer_state_cmp(state, 4));
-   // rgblight_set_layer_state(4, layer_state_cmp(state, 5));
+    rgblight_set_layer_state(1, layer_state_cmp(state, 3));
+    rgblight_set_layer_state(2, layer_state_cmp(state, 4));
+    rgblight_set_layer_state(3, layer_state_cmp(state, 5));
     return state;
 }
